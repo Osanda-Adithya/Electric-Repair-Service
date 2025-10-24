84e22c76dbb5246f512798c14f90ab8d93e5f2e7
Osanda Adithya <osandaadithya@gmail.com>
2025-10-25 01:49:05 +0530

    re apply the lazy loading for all the pages

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,9 +4,6 @@ import LazyLoading from "../components/LazyLoading";
 import Main from "../layouts/Main";
 import NotFound from "../pages/NotFound";
 import { RouteLinks } from "./types";
-import About from "../pages/About";
-import Gallery from "../pages/Gallery";
-import Services from "../pages/Services";
 
 const WebRouter: FC = () => {
     return (
@@ -14,9 +11,9 @@ const WebRouter: FC = () => {
             <Route path={`${RouteLinks.HOME}/:role`} element={<LazyLoading pageName="Admin" />} />
             <Route element={<Main />} >
                 <Route path={RouteLinks.HOME} element={<LazyLoading pageName="Home" />} />
-                <Route path={RouteLinks.SERVICE} element={<Services />} />
-                <Route path={RouteLinks.OURWORKS} element={<Gallery />} />
-                <Route path={RouteLinks.ABOUTUS} element={<About />} />
+                <Route path={RouteLinks.SERVICE} element={<LazyLoading pageName="Services" />} />
+                <Route path={RouteLinks.OURWORKS} element={<LazyLoading pageName="Gallery" />} />
+                <Route path={RouteLinks.ABOUTUS} element={<LazyLoading pageName="About" />} />
             </Route>
             <Route path="*" element={<NotFound />} />
         </Routes>
